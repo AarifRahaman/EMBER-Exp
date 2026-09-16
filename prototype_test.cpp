369b1d357171760bd59cdccdb861567127a26fc7
Aarif Rahaman <arifcse25@gmail.com>
2026-09-16 15:03:54 +0200

    Update fault model prototype

--- prototype_test.cpp
+++ prototype_test.cpp
@@ -5,6 +5,8 @@
 #include <string_view>
 #include <vector>
 #include <variant>
+#include <bitset>
+#include <random>
 
 enum class FaultTiming {            // This is an enum which has predefined three types of window
     Transient,
@@ -120,54 +122,7 @@ class SaboteurBase : public ISaboteur<FaultModels...>{
     }
 };
 
-// Implementation of DPSRAM aka Saboteur
-// This is a concrete dpsram with 3 supported FM, but we can make dpsram templated if we wants something like, dpsram<SEU> a; dpsram<SEU, SA1> b; dpsram<SEU, SA1, SA0> c; user can do both
 
-class dpsram : public SaboteurBase<SEU, SA0, SA1> {
-
-    public:
-    using FaultVariant = SaboteurBase<SEU, SA0, SA1>::FaultVariant;     // for concrete saboteur type, aliases is not actually needed as we inherited Base but here it just make it more understandable to the reader
-
-    
-    size_t totalBw = 64;                // for locations() purpose i declare these two var. Remember everything here is part of a prototype. Right now these are public on purpose.
-    size_t freeSaLocations = 32;
-
-    size_t locations(const FaultVariant& fModel) const override {
-
-        return std::visit(                          // Here comes some complication of using variant. In existing dpsram you used enum model but here i used variant and to retrieve the active model from variant we need to use std::visit.
-                                                    // i feels it works something like roundabout though but for keeping functions virtual , its a good way.
-        
-        [this](const auto& fault) -> size_t{        //This is a lambda function used to get the exact FaultModel instead of something like (const auto& SEU) to return available locations for every particular Fault.
-
-        using T = std::decay_t<decltype(fault)>;    // Here T refers to the exact Fault like "SEU" or "SA1"
-
-        if constexpr (std::same_as<T, SEU>)
-        {
-            return this -> totalBw;
-        }
-        else if constexpr (std::same_as<T, SA0>)
-        {
-            return this -> freeSaLocations;
-        }
-        else if constexpr (std::same_as<T, SA1>)
-        {
-            return this -> freeSaLocations;
-        }
-    }, fModel);
-        
-    }
-
-    void genFaultMask(const FaultVariant&) override {}          // Dummy Implementation
-    void clearFaultMask(const FaultVariant&) override {}
-    void clearAllMasks() override {}
-
-    void applyFault(const FaultVariant&) override {}
-    void applyAllFaults() override {}
-
-    
-};
-
-    
 
 // Example saboteurs
 
@@ -175,11 +130,12 @@ class dpsram : public SaboteurBase<SEU, SA0, SA1> {
 class RegisterSaboteur : public SaboteurBase<SEU, SA0>
 {   
     //Here must need to implement all the remaining virtual functions
-
-    size_t locations(const FaultVariant& fModel) const override       // Dummy Implementation
+    size_t locations(const FaultVariant& fModel) const override
     {
         return 0;
-    }      
+    }
+    
+         
     void genFaultMask(const FaultVariant&) override {}          
     void clearFaultMask(const FaultVariant&) override {}
     void clearAllMasks() override {}
@@ -190,16 +146,332 @@ class RegisterSaboteur : public SaboteurBase<SEU, SA0>
 
 class MemorySaboteur : public SaboteurBase<SEU, SA0, SA1>
 {
-    size_t locations(const FaultVariant& fModel) const override       // Dummy Implementation
-    {
-        return 0;
-    }      
-    void genFaultMask(const FaultVariant&) override {}          
-    void clearFaultMask(const FaultVariant&) override {}
-    void clearAllMasks() override {}
+    public:
+    size_t freesalocations;
+    size_t totalbw;
+    using mask_t = uint8_t;
 
-    void applyFault(const FaultVariant&) override {}
-    void applyAllFaults() override {}
+    mask_t seuMask = 0;
+    mask_t sa1Mask = 0;
+    mask_t sa0Mask = ~mask_t{0};
+
+    uint8_t data = 0;        // Memory location of simulated hardware
+
+    size_t uniformPosition()
+    {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<size_t> dis(0, totalbw - 1);
+
+    return dis(gen);
+    }
+
+    void clearseuMask()
+    {
+        seuMask = 0;
+    }
+
+     void clearsa1Mask()
+    {
+        sa1Mask = 0;
+    }
+
+     void clearsa0Mask()
+    {
+        sa0Mask = ~mask_t{0};
+    }
+
+    
+    void applyseuFault()
+    {
+        data ^= seuMask;
+    }
+    
+    void applysa1Fault()
+    {
+        data |= sa1Mask;
+    }
+    
+    void applysa0Fault()
+    {
+        data &= sa0Mask;
+    }
+
+    
+    size_t locations(const FaultVariant& fModel) const override {
+
+        assert(freesalocations <= totalbw);
+        
+        return std::visit(                          
+                                                    
+        
+        [this](const auto& fault) -> size_t{        
+
+        using T = std::decay_t<decltype(fault)>;    
+
+        if constexpr (std::same_as<T, SEU>)
+        {
+            size_t result = this -> totalbw;
+            
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SEU locations = "
+                        << result << '\n';
+            #endif
+
+            return result;
+        }
+
+        
+        else if constexpr (std::same_as<T, SA0>)
+        {
+            size_t result =  this -> freesalocations;
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SA0 locations = "
+                        << result << '\n';
+            #endif
+
+            return result;
+
+        }
+        else if constexpr (std::same_as<T, SA1>)
+        {
+            size_t result =  this -> freesalocations;
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SA1 locations = "
+                        << result << '\n';
+            #endif
+            
+            return result;
+        }
+    },fModel); 
+
+    }   
+    
+    void genFaultMask(const FaultVariant& fmodel) override 
+    {
+        
+        std::visit(                          
+                                                    
+        
+        [this](const auto& fault) -> void{        
+
+        using T = std::decay_t<decltype(fault)>;    
+
+        size_t pos = uniformPosition();
+
+        if constexpr (std::same_as <T, SEU>)
+        {
+           seuMask |= (mask_t{1} << pos);
+
+           #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << "Generated SEU mask at Position: "
+                        <<pos
+                        <<" & Mask = "
+                        <<std::bitset<8>(seuMask)
+                        <<'\n';
+            #endif
+        }
+
+        else if constexpr (std::same_as <T, SA1>)
+        {
+            sa1Mask |= (mask_t{1} << pos);
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << "Generated SA1 mask at Position: "
+                        <<pos
+                        <<" & Mask = "
+                        <<std::bitset<8>(sa1Mask)
+                        <<'\n';
+            #endif
+        }
+
+         else if constexpr (std::same_as <T, SA0>)
+        {
+            sa0Mask &= ~(mask_t{1} << pos);
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << "Generated SA0 mask at Position: "
+                        <<pos
+                        <<" & Mask = "
+                        <<std::bitset<8>(sa0Mask)
+                        <<'\n';
+            #endif
+        }
+
+       
+        } ,
+        fmodel); 
+    }       
+    void clearFaultMask(const FaultVariant& fmodel) override 
+    {
+        std::visit(                          
+                                                    
+        
+        [this](const auto& fault) -> void{        
+
+        using T = std::decay_t<decltype(fault)>;    
+
+        if constexpr (std::same_as <T, SEU>)
+        {
+           clearseuMask();
+
+           #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SEU Mask Cleared = "
+                        <<std::bitset<8>(seuMask)
+                        <<'\n';
+            #endif
+           
+
+        }
+
+        else if constexpr (std::same_as <T, SA1>)
+        {
+            clearsa1Mask();
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SA1 Mask Cleared = "
+                        <<std::bitset<8>(sa1Mask)
+                        <<'\n';
+            #endif
+        }
+
+         else if constexpr (std::same_as <T, SA0>)
+        {
+            clearsa0Mask();
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SA0 Mask Cleared = "
+                        <<std::bitset<8>(sa0Mask)
+                        <<'\n';
+            #endif
+        }
+
+       
+        } ,
+        fmodel); 
+    }
+    void clearAllMasks() override 
+    {
+        clearsa0Mask();
+        clearsa1Mask();
+        clearseuMask();
+
+        assert(seuMask == 0);
+        assert(sa1Mask == 0);
+        assert(sa0Mask == 0xFF);
+
+        #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": All Mask Cleared "
+                        <<'\n';
+        #endif
+    }
+
+    void applyFault(const FaultVariant& fmodel) override 
+    {
+        std::visit(                          
+                                                    
+        
+        [this](const auto& fault) -> void{        
+
+        using T = std::decay_t<decltype(fault)>;    
+
+        mask_t before = data;
+
+        if constexpr (std::same_as <T, SEU>)
+        {
+            
+            applyseuFault();
+
+            assert(data == (before ^ seuMask));
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SEU applied"
+                        << "\nData before: " << std::bitset<8>(before)
+                        << "\nSEU mask:    " << std::bitset<8>(seuMask)
+                        << "\nData after:  " << std::bitset<8>(data)
+                        << '\n';
+            #endif
+        }
+
+        else if constexpr (std::same_as <T, SA1>)
+        {
+            applysa1Fault();
+
+            assert(data == (before | sa1Mask));
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SA1 applied"
+                        << "\nData before: " << std::bitset<8>(before)
+                        << "\nSA1 mask:    " << std::bitset<8>(sa1Mask)
+                        << "\nData after:  " << std::bitset<8>(data)
+                        << '\n';
+            #endif
+        }
+
+         else if constexpr (std::same_as <T, SA0>)
+        {
+            applysa0Fault();
+
+            assert(data == (before & sa0Mask));
+
+            #ifdef DEBUG
+            std::cout   << "[DEBUG] " << __func__
+                        << ": SA0 applied"
+                        << "\nData before: " << std::bitset<8>(before)
+                        << "\nSA0 mask:    " << std::bitset<8>(sa0Mask)
+                        << "\nData after:  " << std::bitset<8>(data)
+                        << '\n';
+            #endif
+        }
+
+       
+        } ,
+        fmodel); 
+    }
+    void applyAllFaults() override 
+    {
+        mask_t before = data;
+
+        applyseuFault();
+        applysa1Fault();
+        applysa0Fault();
+
+        #ifdef DEBUG
+        std::cout   << "[DEBUG] " << __func__
+                    << ": All faults applied"
+                    << "\nData before: " << std::bitset<8>(before)
+                    << "\nData after:  " << std::bitset<8>(data)
+                    << '\n';
+        #endif
+    }
+
+    //CTOR
+
+    MemorySaboteur(size_t freesa, size_t total)
+    : freesalocations(freesa),
+      totalbw(total)
+{
+    assert(totalbw > 0);
+    assert(totalbw <= 8);
+    assert(freesalocations <= totalbw);
+}
+
+
 };
 
 
@@ -230,7 +502,8 @@ int main()
     
     RegisterSaboteur reg1;
     RegisterSaboteur reg2;
-    MemorySaboteur mem1;
+    MemorySaboteur mem1 (8,8);
+    
     
     
     assert(reg1.faultModelCount() == 2);
@@ -252,119 +525,28 @@ int main()
 
     //End of Previous test
 
+
+    // Example saboteur test.
+
+    MemorySaboteur ex(8,8);
+    ex.data = 0b10100101;
+
+    MemorySaboteur::FaultVariant seu = SEU{};
+    MemorySaboteur::FaultVariant sa0 = SA0{};
+    MemorySaboteur::FaultVariant sa1 = SA1{};
+
+    ex.locations(seu);
+    ex.locations(sa0);
+    ex.locations(sa1);
+
     
-    // Test: FaultVariant type
+    ex.genFaultMask(seu);
+    ex.genFaultMask(sa1);
+    ex.genFaultMask(sa0);
     
-    
-    dpsram ram;
+    ex.applyAllFaults();
 
-    using RamVariant = dpsram::FaultVariant;
-
-    static_assert(std::same_as<RamVariant, std::variant<SEU, SA0, SA1>>);
-
-    std::cout << "FaultVariant tests passed!\n";
-
-
-    // Test: FaultVariant can hold each supported fault model
-
-    RamVariant fault1 = SEU{};
-    RamVariant fault2 = SA0{};
-    RamVariant fault3 = SA1{};
-
-    assert(std::holds_alternative<SEU>(fault1));        // This ensure if currently holding any SEU and correctly remember.
-    assert(std::holds_alternative<SA0>(fault2));
-    assert(std::holds_alternative<SA1>(fault3));
-
-    std::cout << "FaultVariant storage tests passed!\n";
-
-
-    // Test: locations() for SEU
-
-    assert(ram.locations(SEU{}) == 64);
-
-    std::cout << "SEU locations test passed!\n";
-
-
-    // Test: locations() for SA0
-
-    assert(ram.locations(SA0{}) == 32);
-
-    std::cout << "SA0 locations test passed!\n";
-
-
-    // Test: locations() for SA1
-
-    assert(ram.locations(SA1{}) == 32);
-
-    std::cout << "SA1 locations test passed!\n";
-
-
-    // Test: locations() reacts to runtime state
-
-    ram.totalBw = 128;
-    assert(ram.locations(SEU{}) == 128);
-
-    ram.freeSaLocations = 10;
-    assert(ram.locations(SA0{}) == 10);
-    assert(ram.locations(SA1{}) == 10);
-
-    std::cout << "Runtime location-state tests passed!\n";
-
-
-    // Test: faultModelCount() for dpsram
-
-    assert(ram.faultModelCount() == 3);
-
-    std::cout << "dpsram faultModelCount test passed!\n";
-
-
-    // Test: compile-time supports() for dpsram
-
-    static_assert(dpsram::supports<SEU>());
-    static_assert(dpsram::supports<SA0>());
-    static_assert(dpsram::supports<SA1>());
-    static_assert(!dpsram::supports<FaultX>());
-
-    std::cout << "dpsram supports tests passed!\n";
-
-
-    // Test: access through ISaboteur base pointer
-    // This specially checking virtual function behaviour.
-
-    ISaboteur<SEU, SA0, SA1>* sab = &ram;
-
-    RamVariant seuFault = SEU{};
-    RamVariant sa0Fault = SA0{};
-    RamVariant sa1Fault = SA1{};
-
-    ram.totalBw = 64;
-    ram.freeSaLocations = 32;
-
-    assert(sab->locations(seuFault) == 64);
-    assert(sab->locations(sa0Fault) == 32);
-    assert(sab->locations(sa1Fault) == 32);
-
-    std::cout << "Virtual dispatch tests passed!\n";
-
-
-    // Test: variant changes active fault at runtime
-
-    RamVariant currentFault = SEU{};
-
-    assert(std::holds_alternative<SEU>(currentFault));
-    assert(ram.locations(currentFault) == 64);
-
-    currentFault = SA0{};
-
-    assert(std::holds_alternative<SA0>(currentFault));
-    assert(ram.locations(currentFault) == 32);
-
-    currentFault = SA1{};
-
-    assert(std::holds_alternative<SA1>(currentFault));
-    assert(ram.locations(currentFault) == 32);
-
-    std::cout << "Variant switching tests passed!\n";
+    ex.clearAllMasks();
 
 
         
